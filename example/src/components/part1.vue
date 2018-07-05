605140bb4f30582a32811de1d111aafe78b715d8
ZHKO1 <zhko11993@gmail.com>
2018-07-05 16:12:24 +0800

    第三章完毕

--- example/src/components/part1.vue
+++ example/src/components/part1.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <div class="container">
-      <p class="p_type1">To stop the degradation of the planet's natural environment and to build a future in which humans live in harmony with nature, by; conserving the world's biological diversity, ensuring that the use of renewable natural resources is sustainable, and promoting the reduction of pollution and wasteful consumption.</p>
+      <p class="p_type1">To stop the degradation of the planet's natural environment and to build a future in which
+        humans live in harmony with nature, by; conserving the world's biological diversity, ensuring that the use of
+        renewable natural resources is sustainable, and promoting the reduction of pollution and wasteful
+        consumption.</p>
       <p class="p_type2">FUCK YOU</p>
       <code>
         #shit2+span{
@@ -30,13 +33,15 @@
       <strong>display:inline-block;</strong>
       <div class="box inline-block">
         <img src="../assets/logo.png">
-        <p>display:inline-block具有收缩特性，但是，当（例如这里的）描述文字超过一行显示的时候，其会这行，不会让自身的宽度超过父级容器的可用空间的，但是，width:max-content就不是酱样子哦咯！表现得好像设置了white-space:nowrap一样，科科！</p>
+        <p>
+          display:inline-block具有收缩特性，但是，当（例如这里的）描述文字超过一行显示的时候，其会这行，不会让自身的宽度超过父级容器的可用空间的，但是，width:max-content就不是酱样子哦咯！表现得好像设置了white-space:nowrap一样，科科！</p>
       </div>
 
       <strong>width: max-content;</strong>
       <div class="box max-content">
         <img src="../assets/logo.png">
-        <p>display:inline-block具有收缩特性，但是，当（例如这里的）描述文字超过一行显示的时候，其会这行，不会让自身的宽度超过父级容器的可用空间的，但是，width:max-content就不是酱样子哦咯！表现得好像设置了white-space:nowrap一样，科科！</p>
+        <p>
+          display:inline-block具有收缩特性，但是，当（例如这里的）描述文字超过一行显示的时候，其会这行，不会让自身的宽度超过父级容器的可用空间的，但是，width:max-content就不是酱样子哦咯！表现得好像设置了white-space:nowrap一样，科科！</p>
       </div>
 
       <strong>width: min-content;</strong>
@@ -80,7 +85,8 @@
           <input id="check1" type="checkbox">
           <p>个人觉得，display:table-cell最强的应用是可以任意个数列表的等宽效果。</p>
           <div class="element">
-            <p>display:table-cell其他一些应用，例如，两栏自适应布局，垂直居中效果等等都是可以通过其他技术手段模拟出来的，但是，根据列表个数自动等宽的效果，其他CSS是很难模拟的，尤其当需要兼容IE8浏览器的时候。</p>
+            <p>
+              display:table-cell其他一些应用，例如，两栏自适应布局，垂直居中效果等等都是可以通过其他技术手段模拟出来的，但是，根据列表个数自动等宽的效果，其他CSS是很难模拟的，尤其当需要兼容IE8浏览器的时候。</p>
           </div>
           <label for="check1" class="check-in">更多↓</label>
           <label for="check1" class="check-out">收起↑</label>
@@ -89,7 +95,8 @@
           <input id="check2" type="checkbox">
           <p>个人觉得，display:table-cell最强的应用是可以任意个数列表的等宽效果。</p>
           <div class="element">
-            <p>display:table-cell其他一些应用，例如，两栏自适应布局，垂直居中效果等等都是可以通过其他技术手段模拟出来的，但是，根据列表个数自动等宽的效果，其他CSS是很难模拟的，尤其当需要兼容IE8浏览器的时候。</p>
+            <p>
+              display:table-cell其他一些应用，例如，两栏自适应布局，垂直居中效果等等都是可以通过其他技术手段模拟出来的，但是，根据列表个数自动等宽的效果，其他CSS是很难模拟的，尤其当需要兼容IE8浏览器的时候。</p>
             <p>然而，此方法也有局限性，就是只能实现单行列表的等分，所以，如果我们希望列表数目超过一定值的时候变成多行，就需要根据数目不同，输出不同的DOM结构，仅仅靠CSS有难度。</p>
           </div>
           <label for="check2" class="check-in">更多↓</label>
@@ -100,52 +107,73 @@
     <div class="container" style="width:50%">
       <div style="background:yellow"><span style="display: inline-block"></span></div>
     </div>
+
+    <div class="container" style="width:50%;">
+      <div style="background: cornflowerblue;line-height: 400px;font-size: 0;"><img src="../assets/makelei.png"
+                                                                                    style="vertical-align: middle;">
+      </div>
+    </div>
+
+    <div class="container">
+      <div style="text-align: justify;max-width:500px;background-color:#E5EDFF;line-height: 0;" class="outline">
+        <img src="../assets/makelei.png" style="width:130px;height:120px;display: inline-block">
+        <img src="../assets/makelei.png" style="width:130px;height:120px;display: inline-block">
+        <img src="../assets/makelei.png" style="width:130px;height:120px;display: inline-block">
+        <img src="../assets/makelei.png" style="width:130px;height:120px;display: inline-block">
+        <span class="justify-fix"></span>
+        <span class="justify-fix"></span>
+        <span class="justify-fix"></span>FUCKYOU
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'part1',
-  methods:{
-    clickevent:function(e){
-      e.preventDefault();
+  export default {
+    name: 'part1',
+    methods: {
+      clickevent: function (e) {
+        e.preventDefault();
+      },
     },
-  },
-  data () {
-    return {
-      items:[
-        1,2,3,4,5,7,8,9,10
-      ]
+    data () {
+      return {
+        items: [
+          1, 2, 3, 4, 5, 7, 8, 9, 10
+        ]
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .container{
-    border:1px solid black;
+  .container {
+    border: 1px solid black;
     margin-bottom: 20px;
   }
-  #number2+span{
-    color:green
+
+  #number2 + span {
+    color: green
   }
-  #number8~span{
-    color:blue
+
+  #number8 ~ span {
+    color: blue
   }
-  p.p_type1:first-line
-  {
-    background-color:yellow;
+
+  p.p_type1:first-line {
+    background-color: yellow;
   }
-  p.p_type2:first-letter
-  {
-    background-color:yellow;
+
+  p.p_type2:first-letter {
+    background-color: yellow;
   }
 
   .active-btn {
     display: inline-block;
     background-color: white;
   }
+
   .active-btn:active {
     background-color: yellow;
   }
@@ -159,7 +187,7 @@ export default {
     vertical-align: middle;
     width: -webkit-fill-available;
     width: -moz-fill-available;
-    width: -moz-available;    /* FireFox目前这个生效 */
+    width: -moz-available; /* FireFox目前这个生效 */
     width: fill-available;
   }
 
@@ -174,20 +202,24 @@ export default {
   .inline-block {
     display: inline-block;
   }
+
   .max-content {
     width: -webkit-max-content;
     width: -moz-max-content;
     width: max-content;
   }
+
   .min-content {
     width: -webkit-min-content;
     width: -moz-min-content;
     width: min-content;
   }
-  #minContentIMG:hover{
-    width:50px;
+
+  #minContentIMG:hover {
+    width: 50px;
   }
-  .fit-content{
+
+  .fit-content {
     width: -webkit-fit-content;
     width: -moz-fit-content;
     width: fit-content;
@@ -202,25 +234,31 @@ export default {
     margin: 35px;
     color: #fff;
   }
+
   .ao:before,
   .tu:before {
     outline: 2px solid #cd0000;
     font-family: Consolas, Monaco, monospace;
   }
+
   .ao:before {
     content: "fuck你fuck";
   }
+
   .tu {
     direction: rtl;
   }
+
   .tu:before {
     content: "我fuck你";
   }
+
   .box_style1 {
     display: inline-block;
     white-space: nowrap;
     background-color: #cd0000;
   }
+
   .box_style1 .text {
     display: inline-block;
     width: 100%;
@@ -228,6 +266,7 @@ export default {
     color: #fff;
     text-align: left
   }
+
   .box_style2 {
     width: 200px;
     height: 160px;
@@ -235,19 +274,23 @@ export default {
     box-sizing: border-box;
     background-color: #beceeb;
     border: 2px solid black;
-    margin:0 auto 40px auto;
+    margin: 0 auto 40px auto;
   }
+
   .box_style2 .child {
     height: 100%;
     background: #cd0000;
   }
+
   .box_style2.rel {
     position: relative;
   }
+
   .box_style2.rel > .child {
     width: 100%;
     position: absolute;
   }
+
   img.img_type1 {
     max-width: 256px;
   }
@@ -261,6 +304,7 @@ export default {
     table-layout: fixed;
     text-align: left;
   }
+
   .table .td {
     display: table-cell;
     padding: 5px;
@@ -270,26 +314,41 @@ export default {
     position: absolute;
     clip: rect(0 0 0 0);
   }
+
   .table .check-in,
   .check-out {
     color: #34538b;
     cursor: pointer;
   }
+
   .table .check-out {
     display: none;
   }
+
   .table :checked ~ .check-out {
     display: inline-block;
   }
+
   .table :checked ~ .check-in {
     display: none;
   }
+
   .table .element {
     max-height: 0;
     overflow: hidden;
     transition: max-height .25s;
   }
+
   .table :checked ~ .element {
     max-height: 666px;
+  }
+
+  .justify-fix {
+    display: inline-block;
+    width: 128px;
+  }
+
+  .outline .justify-fix {
+    outline: 1px dashed red;
   }
 </style>
