64c4445a1d71d3b8c869fbaa90e37183eb34b3b4
ZHKO1 <zhko11993@gmail.com>
2018-08-08 17:48:29 +0800

    继续第五章

--- example/src/components/part3.vue
+++ example/src/components/part3.vue
@@ -67,6 +67,75 @@
         <p class="cell cell-2"><img src="../assets/makelei.png"></p>
       </div>
     </div>
+    <div class="container type10">
+      <h6>vertical-align和line-height之间的关系（容器高度不等于行高）</h6>
+      <h6>解决方案1：让幽灵空白节点和后面span元素字号一样大</h6>
+      <h6>解决方案2：改变垂直对齐方式</h6>
+      <div class="box">
+        x<span>文本</span>
+      </div>
+    </div>
+    <div class="container type11">
+      <h6>text-align:justify例子</h6>
+      <div class="box">
+        <img src="../assets/makelei.png" width="96" alt="">
+        <img src="../assets/makelei.png" width="96" alt="">
+        <img src="../assets/makelei.png" width="96" alt="">
+        <img src="../assets/makelei.png" width="96" alt="">
+        <i class="justify-fix">&nbsp;</i>
+        <i class="justify-fix">&nbsp;</i>
+        <i class="justify-fix">&nbsp;</i>
+      </div>
+    </div>
+    <div class="container type12">
+      <h6>inline-block与baseline的关系</h6>
+      <div class="box" style="line-height: 3px">
+        x<i class="justify-fix"></i>
+        <i class="justify-fix" style="overflow: hidden">xyz</i>
+      </div>
+    </div>
+    <div class="container type13">
+      <h6>利用inline-block基线原理的小图标对齐实例页面</h6>
+      <div class="box">
+        <h6>1. 空标签后面跟随文本</h6>
+        <p><i class="icon icon-delete"></i>删除</p>
+        <h6>2. 标签里面有“删除”文本</h6>
+        <p><i class="icon icon-delete">删除</i>随便什么文字</p>
+        <h6>3. 字号变大</h6>
+        <p class="large">
+          <i class="icon icon-delete"></i>删除
+        </p>
+        <p class="large">
+          <i class="icon icon-delete">删除</i>随便什么文字
+        </p>
+      </div>
+    </div>
+    <div class="container type14">
+      <h6>没咋意义，就是想看看容器不设定高度，纯设置line-height，内i标签以及文本字体大小会不会改变高度</h6>
+      <div class="box" style="border:1px solid red;line-height: 20px;font-size: 30px;">
+        <i style="width:20px;height:20px;">xy</i>SHIT
+      </div>
+    </div>
+    <div class="container type15">
+      <h6>vertial-align:middle与近似垂直居中</h6>
+      <div id="box" class="box" style="font-size: 48px;">
+        <span class="wrap"><img src="../assets/makelei.png"></span>x
+      </div>
+    </div>
+    <div class="container type16">
+      <h6>vertical-align文本类属性值深入实例,分别设置为16px 24px 32px试试看</h6>
+      <div class="box box-1" style="font-size: 16px;">
+        <span class="wrap"><img src="../assets/makelei.png"></span>
+        <span class="f16">16px</span><span class="f24">24px</span><span class="f32">32px</span>
+      </div>
+    </div>
+    <div class="container type17">
+      <h6>基于vertical-align属性的水平垂直居中弹框</h6>
+      <div class="button"><button @click="type17Click">ROW!ROW!FIGHT THE POWER!</button></div>
+      <div class="container" id="type17" style="display: none" @click="type17Click">
+        <span style="color:white;font-size: 14px;">xyz</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -75,6 +144,19 @@
   export default {
     name: 'part3',
     methods: {
+      type17Click: function(event){
+        //肯定还有更加优雅的方式，见鬼了
+        var div = document.getElementById("type17");
+        if(div.style.display == "none"){
+          div.style.display = "block"
+        }else{
+          if(event.srcElement.id == "type17"){
+            div.style.display = "none"
+          }
+        }
+        event=event||window.event;
+        event.stopPropagation();
+      },
     },
     data () {
       return {
@@ -94,6 +176,9 @@
     h6{
       margin:0.5em;
     }
+    &&:last-child{
+    margin-bottom: 0px;
+     }
   }
   .type1{
     font-size: 14px;
@@ -223,6 +308,181 @@
     }
     .cell-2 {
       vertical-align: middle;
+    }
+  }
+  .type10{
+    .box {
+      display: inline-block;
+      line-height: 32px;
+      border: 1px solid red;
+    }
+    .box > span {
+      font-size: 24px;
+      background:yellow;
+    }
+  }
+  .type11{
+    .box {
+      width: 300px;
+      text-align: justify;
+      border:1px solid red;
+      margin:auto;
+      line-height: 0px;
+    }
+    .justify-fix{
+      display: inline-block;
+      width:96px;
+      outline: 1px solid red;
+    }
+  }
+  .type12{
+    .box {
+      width: 300px;
+      border:1px solid red;
+      margin:auto;
+      line-height: 0px;
+    }
+    .justify-fix{
+      display: inline-block;
+      width:96px;
+      outline: 1px solid red;
+    }
+  }
+  .type13{
+    .box {
+      display: inline-block;
+      text-align: left;
+      line-height: 20px;
+    }
+    .icon {
+      display: inline-block;
+      width:20px; height:20px;
+      white-space: nowrap;
+      letter-spacing: -1em;
+      text-indent: -999em;
+    }
+    .icon:before {
+      content: '\3000';
+    }
+    .icon-delete {
+      background: url(../assets/makelei.png) no-repeat center/20px 20px;
+    }
+    .large {
+      font-size: 30px;
+    }
+  }
+  .type15{
+    .box {
+      width: 280px;
+      line-height: 128px;
+      margin: auto;
+      background-color: #f0f3f9;
+      font-size: 16px;
+      position: relative;
+    }
+    .box img {
+      height: 96px;
+      vertical-align: middle;
+    }
+    .wrap {
+      display: inline-block;
+      line-height: 0;
+      color: red;
+      position: relative;
+    }
+    .wrap:before,
+    .box:after {
+      content: '';
+      position: absolute;
+      top: 50%; left: 0; right: 0;
+      border-top: 1px solid;
+    }
+    .box:after {
+      color: green;
+    }
+  }
+  .type16{
+    .box {
+      width: 300px;
+      margin: auto;
+      background-color: #f0f3f9;
+      text-align: left;
+      font-size: 16px;
+      white-space: nowrap;
+      position: relative;
+    }
+    .box img {
+      height: 96px;
+    }
+    .box-1 img {
+      vertical-align: text-top;
+    }
+    .box-2 img {
+      vertical-align: text-bottom;
+    }
+    .wrap {
+      display: inline-block;
+      line-height: 0;
+      color: red;
+    }
+    .wrap:before,
+    .wrap:after {
+      content: '';
+      display: none;
+      position: absolute;
+      left: 0;
+      right: 0;
+      border-top: 1px solid;
+    }
+    .box-1 .wrap:before {
+      display: block;
+    }
+    .box-2 .wrap:after {
+      display: block;
+    }
+    .f16 { font-size: 16px; }
+    .f24 { font-size: 24px; }
+    .f32 { font-size: 32px; }
+
+    .f16, .f24, .f32 {
+      background-color: #308CFE;
+      color: #fff;
+    }
+  }
+  .type17{
+    .container {
+      position: fixed;
+      top: 0; right: 0; bottom: 0; left: 0;
+      background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAABtJREFUeNpiZGBgaGAgAjAxEAlGFVJHIUCAAQDcngCUgqGMqwAAAABJRU5ErkJggg==);
+      background: rgba(0,0,0,.5), none;
+      text-align: center;
+      white-space: nowrap;
+      font-size: 0;
+      z-index: 99;
+    }
+    .container:after {
+      content: "";
+      display: inline-block;
+      height: 100px;
+      width:2px;
+      background:yellow;
+    }
+    span{
+      vertical-align: top;
+    }
+    .dialog {
+      display: inline-block;
+      border-radius: 6px;
+      vertical-align: middle;
+      background-color: #fff;
+      font-size: 14px;
+      text-align: left;
+      white-space: normal;
+    }
+    .content {
+      width: 240px;
+      height: 120px;
+      padding: 20px;
     }
   }
 </style>
